59b10c2f16f2865c6824d48ad0a508a7e2f46013
Israel Lopes <ilsrael0406@gmail.com>
2023-05-30 20:26:27 -0300

    at

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+
+
 $(document).ready(function() {
     $('#contactForm').submit(function(e) {
       e.preventDefault(); // Impede o envio padrão do formulário
-
+   
       // Envia o formulário usando AJAX
       $.ajax({
         url: $(this).attr('action'),
@@ -38,4 +40,64 @@ $(document).ready(function() {
       header.classList.remove('header-scroll');
     }
   });
+
+
+  
+
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    const carousels = [
+      { id: 'carousel1', folder: './img/galerys/galery1/', imageCount: 39 },
+      { id: 'carousel2', folder: './img/galerys/galery2/', imageCount: 21 },
+      { id: 'carousel3', folder: './img/galerys/galery3/', imageCount: 17 },
+      { id: 'carousel4', folder: './img/galerys/galery4/', imageCount: 15 },
+      { id: 'carousel5', folder: './img/galerys/galery5/', imageCount: 20 },
+      { id: 'carousel6', folder: './img/galerys/galery6/', imageCount: 12 },
+      { id: 'carousel7', folder: './img/galerys/galery7/', imageCount: 5 },
+      { id: 'carousel8', folder: './img/galerys/galery8/', imageCount: 10 }
+    ];
+    // Função para mudar a imagem de fundo da div
+  // Função para mudar a imagem de fundo da div
+  function changeBackgroundImage(carouselId, folder, imageIndex) {
+    const imagePath = `${folder}${imageIndex}.webp`;
+    const carousel = document.getElementById(carouselId);
+    carousel.style.backgroundImage = `url(${imagePath})`;
+  }
+
+  // Função para ampliar o carrossel
+  function zoomCarousel(event) {
+    const carousel = event.target.parentElement.parentElement;
+    carousel.classList.toggle('zoomed');
+  }
+
+  // Adiciona o evento de clique aos botões e à imagem
+  carousels.forEach(carousel => {
+    const prevBtn = document.querySelector(`#${carousel.id} .prev-btn`);
+    const nextBtn = document.querySelector(`#${carousel.id} .next-btn`);
+    const image = document.querySelector(`#${carousel.id} .carousel-image`);
+    const closeBtn = document.querySelector(`#${carousel.id} .close-btn`);
+
+    let currentImageIndex = 1;
+
+    prevBtn.addEventListener('click', () => {
+      currentImageIndex = (currentImageIndex - 1 + carousel.imageCount) % carousel.imageCount;
+      changeBackgroundImage(carousel.id, carousel.folder, currentImageIndex + 1);
+    });
+
+    nextBtn.addEventListener('click', () => {
+      currentImageIndex = (currentImageIndex + 1) % carousel.imageCount;
+      changeBackgroundImage(carousel.id, carousel.folder, currentImageIndex + 1);
+    });
+
+    image.addEventListener('click', zoomCarousel);
+
+    closeBtn.addEventListener('click', () => {
+      carousel.classList.remove('zoomed');
+    });
+
+    // Define a imagem inicial
+    changeBackgroundImage(carousel.id, carousel.folder, currentImageIndex);
+  });
+});
+  
   
